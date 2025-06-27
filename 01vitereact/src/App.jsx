484564e83a167600c87ce4cd8fcd6ce442961572
nevil811 <nevilbarick69@gmail.com>
2025-06-27 12:09:21 +0530

    Update README.md

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,12 +2,12 @@ import Chai from "./chai";
 
 
 function App() {
-  
+  const username = "With Hitesh Choudhary"
 
   return (
     <>
    <Chai/>
-   
+   <h1>Chai aur React {username} </h1>
 </>
   )}
   export default App;
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,18 @@
+import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+const reactElement = React.createElement(
+  'a',
+  {href:'https://www.google.com',
+    target :'_blank'},
+    'click me to visit google'
+  
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  
+<App/>
+  
 )
