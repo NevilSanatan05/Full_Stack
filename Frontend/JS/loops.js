1c9014c405a3226ee78acbe5a5c87530e882b118
nevil811 <nevilbarick69@gmail.com>
2025-08-16 11:56:17 +0530

    loops

---
+++ Frontend/JS/loops.js
@@ -0,0 +1,43 @@
+// for(i=0;i<=5;i++){
+//   console.log('Current number is: ' + i);
+
+const { styleText } = require("util");
+
+  
+// }
+
+
+// let i =0
+// while (i <= 5) {
+//   console.log('Current number is: ' + i);
+//   i++;
+// }
+
+// let i = 0;
+// do {
+//   console.log('Current number is: ' + i);
+//   i++;
+// } while (i <= 5);
+
+// let fruits = ["Apple", "Banana", "Cherry"];
+// for (const fruit of fruits) {
+//   console.log('Current fruit is: ' + fruit);
+// }
+
+// let student = {
+//   name: "John",
+//   age: 20,
+//   grade: "A"
+// };
+// for (const key in student) {
+//   console.log(key + " : " + student[key]);
+// }
+
+
+// console.log(student.name);
+
+// let array = [1, 2, 3, 4, 5];
+// array.forEach((num,index)=> {
+//   console.log(index+ " : " +num);
+  
+// });
