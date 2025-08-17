b1b59cb64c178167d18ff5d4940aff84ef5f436b
nevil811 <nevilbarick69@gmail.com>
2025-08-17 12:33:08 +0530

    ES6

---
+++ Frontend/JS/ES6/arrays.js
@@ -0,0 +1,13 @@
+const numbers = [1, 2, 3, 4, 5];
+// const squares = numbers.map(num => num * num);
+// console.log(squares); // Output: [1, 4, 9, 16, 25]
+// const evenNumbers = numbers.filter(num => num % 2 === 0);
+// console.log('Even Numbers:', evenNumbers); // Output: [2, 4]
+
+// const total = numbers.reduce((acc, num) => acc + num, 0);
+// console.log('Total:', total); // Output: 15
+
+// numbers.forEach(num => console.log(num * 2)); // Output: 2, 4, 6, 8, 10
+
+// const found = numbers.find(num => num > 3);
+// console.log('First number greater than 3:', found); // Output: 4
---
+++ Frontend/JS/ES6/index.js
@@ -0,0 +1,17 @@
+// const name = "John Doe";
+// console.log('Hello ', name);
+
+
+// const numbers = [1, 2, 3, 4, 5];
+// const [first,second]=numbers;
+// console.log(first, second); 
+
+// const arr1 = [1, 2, 3];
+// const arr2 = [4, 5, 6];
+// const combined = [...arr1, ...arr2];
+// console.log(combined); // Output: [1, 2, 3, 4, 5, 6]
+
+// function sum(...nums) {
+//   return nums.reduce((acc, val) => acc + val, 0);
+// }
+// console.log(sum(1, 2, 3, 4));
